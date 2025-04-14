19f0c8cd2ce62593b213fa9dbc524bf4f9950e74
Nam Pham <namphamcse@gmail.com>
2025-04-14 17:52:19 +0700

    scheduling first commit

--- src/queue.c
+++ src/queue.c
@@ -7,14 +7,23 @@ int empty(struct queue_t * q) {
 	return (q->size == 0);
 }
 
-void enqueue(struct queue_t * q, struct pcb_t * proc) {
-        /* TODO: put a new process to queue [q] */
+void enqueue(struct queue_t *q, struct pcb_t *proc) {
+        if (q->size < MAX_QUEUE_SIZE) {
+            q->proc[q->size] = proc;
+            q->size++;
+        }
 }
+     
 
-struct pcb_t * dequeue(struct queue_t * q) {
-        /* TODO: return a pcb whose prioprity is the highest
-         * in the queue [q] and remember to remove it from q
-         * */
-	return NULL;
+
+struct pcb_t *dequeue(struct queue_t *q) {
+        if (q->size == 0) return NULL;
+        struct pcb_t *proc = q->proc[0];
+        for (int i = 1; i < q->size; i++) {
+            q->proc[i - 1] = q->proc[i];
+        }
+        q->size--;
+        return proc;
 }
+    
 
